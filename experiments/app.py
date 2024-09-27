8e8eb350007e958d66f142e2a36dce05db380aca
kirankm <kiran.k.madan@gmail.com>
2024-09-27 10:13:56 +0530

    Added Dynamic Data

--- experiments/app.py
+++ experiments/app.py
@@ -1,11 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        "id":1,
+        "title":"Data Analyst",
+        "location":"Bengaluru",
+        "salary": 'Rs 1000000'
+    },
+    {
+        "id":2,
+        "title":"Data Scientist",
+        "location":"Delhi",
+        "salary": 'Rs 1500000'
+    },
+    {
+        "id":3,
+        "title":"Front End Engineer",
+        "location":"Remote"
+    },
+    {
+        "id":4,
+        "title":"Back End Engineer",
+        "location":"San Fransisco",
+        "salary": '$ 100000'
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs = JOBS)
 
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(debug = True)
